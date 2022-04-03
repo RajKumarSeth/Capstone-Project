313ca385ff2f068a3fa6372cf63e794ecf950dfc
RajKumarSeth <68587188+RajKumarSeth@users.noreply.github.com>
2022-04-03 12:55:44 +0530

    third commit

--- sim/fft_4point.v
+++ sim/fft_4point.v
@@ -17,7 +17,7 @@ D_FF dff2(.d(w9),.clk(clk),.clear(clear),.q(w3));
 
 DeMux1to2 demux2(.a(w3),.sel(sel_1[1]),.y1(w8),.y2(w6));
 butterfly but(.a(w8),.b(w2),.y1(w4),.y2(w5));
-multiplier ml(.d(w6),.c(c),.y(w11));
+booth_top ml(.M(w6),.Q(c),.Z(w11));
 Mux2to1 mux(.a(w4),.b(w11),.sel(sel_1[1]),.y(w10));
 delay point_2fft(.a(w10),.sel(sel[0]),.clk(clk),.clear(clear),.y(y));
 endmodule
